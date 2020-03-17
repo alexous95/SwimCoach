97e7e2051188a3e8c1064626e54af5d000de74cf
alexous95 <alexandre95870@yahoo.fr>
2020-03-17 11:36:58 +0100

    add a class to manager FireStore read and write data

--- SwimCoach/Services/FirestoreManager.swift
+++ SwimCoach/Services/FirestoreManager.swift
@@ -7,11 +7,45 @@
 //
 
 import Foundation
-import Firebase
+import FirebaseAuth
 
-class FirestoreManager {
+final class FirestoreManager {
     
-    func fetchGroupe(completion: @escaping (Group?, Error) -> () ) {
+    /// Fetch the documents from the group's collection
+    /// - Parameter completion: An escaping closure of type ([Group], Error?) to pass data to another class
+    /// We use this class to populate our model in the viewModel
+    static func fetchGroupe(completion: @escaping ([Group], Error?) -> () ) {
+        var groups = [Group]()
         
+        if let user = Auth.auth().currentUser {
+            let ref = FirestoreService.database.collection("users").document(user.uid)
+            
+            ref.collection("groups").getDocuments { (snapshot, error) in
+                if error != nil {
+                    completion(groups, error!)
+                    return
+                }
+                
+                guard let snapshot = snapshot else { return }
+                let documents = snapshot.documents
+                
+                for document in documents {
+                    guard let group = Group(document: document.data()) else { return }
+                    groups.append(group)
+                }
+                completion(groups, nil)
+            }
+        }
+    }
+    
+    /// Add a document to the group's collection
+    /// - Parameter group: The object that will be added to the database
+    ///
+    /// The group model has a property dictionnary of type [String : Any] used to add data to firestore
+    static func addGroup(group: Group) {
+        if let user = Auth.auth().currentUser{
+            let ref = FirestoreService.database.collection("users").document(user.uid).collection("groups")
+             ref.document(group.groupName).setData(group.dictionnary)
+        }
     }
 }
