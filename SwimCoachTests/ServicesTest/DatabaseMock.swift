cb092f9cc2b36b07330b8b0656ee602450ff02b4
alexous95 <alexandre95870@yahoo.fr>
2020-04-18 09:27:04 +0200

    Refactor test classes

---
+++ SwimCoachTests/ServicesTest/DatabaseMock.swift
@@ -0,0 +1,85 @@
+//
+//  DatabaseMock.swift
+//  SwimCoachTests
+//
+//  Created by Alexandre Goncalves on 18/04/2020.
+//  Copyright © 2020 Alexandre Goncalves. All rights reserved.
+//
+
+import Foundation
+@testable import SwimCoach
+
+class DatabaseMock {
+    
+    var emptyWorkoutLine = [WorkoutLine]()
+    var databaseWorkout = [String : [String: [Workout]]]()
+    var databaseWorkoutLines =  [String: [WorkoutLine]]()
+    var error: Error?
+    var group: Group = Group(groupName: "")
+    
+    enum DatabaseError: Error {
+        case empty
+    }
+    
+    private func createWorkoutLines() -> [WorkoutLine] {
+        let workoutLine = WorkoutLine(text: "test", zone1: 1.0, zone2: 1.0, zone3: 1.0, zone4: 1.0, zone5: 1.0, zone6: 1.0, zone7: 1.0, ampM: 1.0, coorM: 1.0, endM: 1.0, educ: 1.0, crawl: 1.0, medley: 1.0, spe: 1.0, nageC: 1.0, jbs: 1.0, bras: 1.0, workoutLineID: "testID", workoutLineTitle: "titleTest")
+        
+        let workoutLine2 = WorkoutLine(text: "test2", zone1: 1.0, zone2: 1.0, zone3: 1.0, zone4: 1.0, zone5: 1.0, zone6: 1.0, zone7: 1.0, ampM: 1.0, coorM: 1.0, endM: 1.0, educ: 1.0, crawl: 1.0, medley: 1.0, spe: 1.0, nageC: 1.0, jbs: 1.0, bras: 1.0, workoutLineID: "testID2", workoutLineTitle: "titleTest2")
+        
+        let workoutLines = [workoutLine, workoutLine2]
+        
+        return workoutLines
+    }
+    
+    private func createWorkouts() -> [Workout] {
+        
+        let workoutLines = createWorkoutLines()
+        var databaseWorkoutLines = [String : [WorkoutLine]]()
+        
+        var workouts = [Workout]()
+        
+        for index in 0...2 {
+            let workout = Workout(title: "testTitle" + "\(index)", date: "testDate" + "\(index)", workoutID: "testID" + "\(index)", workoutLines: workoutLines)
+            workouts.append(workout)
+            
+            databaseWorkoutLines[workout.workoutID] = workoutLines
+        }
+        
+        return workouts
+    }
+
+    private func createWorkoutDatabase() -> [String : [String : [Workout]]] {
+        let workout = createWorkouts()
+        
+        let databaseWorkout = ["Arctique" : ["April" : workout]]
+        
+        return databaseWorkout
+    }
+    
+    private func createWorkoutLinesDatabase() -> [String: [WorkoutLine]] {
+        
+        let workoutLines = createWorkoutLines()
+        var databaseWorkoutLines = [String : [WorkoutLine]]()
+        
+        for index in 0...2 {
+            databaseWorkoutLines["testID" + "\(index)"] = workoutLines
+        }
+        
+        return databaseWorkoutLines
+    }
+    
+    private func createGroup() -> Group {
+        return Group(groupName: "Arctique")
+    }
+    
+    private func createError() -> DatabaseError {
+        return .empty
+    }
+    
+    func initDatabase() {
+        databaseWorkout = createWorkoutDatabase()
+        databaseWorkoutLines = createWorkoutLinesDatabase()
+        error = createError()
+        group = createGroup()
+    }
+}
--- SwimCoachTests/ServicesTest/Training/FirestoreWorkoutTest.swift
+++ SwimCoachTests/ServicesTest/Training/FirestoreWorkoutTest.swift
@@ -11,95 +11,27 @@ import XCTest
 
 class FirestoreWorkoutTest: XCTestCase {
 
-    enum WorkoutError: Error {
-        case empty
-    }
-    
-    var databaseWorkout: [String : [String: [Workout]]]!
-    var databaseWorkoutLines: [String: [WorkoutLine]]!
-    var error: WorkoutError!
-    var group: Group!
+    var database = DatabaseMock()
     var workoutManager: FirestoreWorkoutMock!
     var workoutManagerError: FirestoreWorkoutMock!
     
-    func createWorkoutLines() -> [WorkoutLine] {
-        let workoutLine = WorkoutLine(text: "test", zone1: 1.0, zone2: 1.0, zone3: 1.0, zone4: 1.0, zone5: 1.0, zone6: 1.0, zone7: 1.0, ampM: 1.0, coorM: 1.0, endM: 1.0, educ: 1.0, crawl: 1.0, medley: 1.0, spe: 1.0, nageC: 1.0, jbs: 1.0, bras: 1.0, workoutLineID: "testID", workoutLineTitle: "titleTest")
-        
-        let workoutLine2 = WorkoutLine(text: "test2", zone1: 1.0, zone2: 1.0, zone3: 1.0, zone4: 1.0, zone5: 1.0, zone6: 1.0, zone7: 1.0, ampM: 1.0, coorM: 1.0, endM: 1.0, educ: 1.0, crawl: 1.0, medley: 1.0, spe: 1.0, nageC: 1.0, jbs: 1.0, bras: 1.0, workoutLineID: "testID2", workoutLineTitle: "titleTest2")
-        
-        let workoutLines = [workoutLine, workoutLine2]
-        
-        return workoutLines
-    }
-    
-    func createWorkouts() -> [Workout] {
-        
-        let workoutLines = createWorkoutLines()
-        var databaseWorkoutLines = [String : [WorkoutLine]]()
-        
-        var workouts = [Workout]()
-        
-        for index in 0...2 {
-            let workout = Workout(title: "testTitle" + "\(index)", date: "testDate" + "\(index)", workoutID: "testID" + "\(index)", workoutLines: workoutLines)
-            workouts.append(workout)
-            
-            databaseWorkoutLines[workout.workoutID] = workoutLines
-        }
-        
-        return workouts
-    }
-    
-    
-    func createWorkoutDatabase() -> [String : [String : [Workout]]] {
-        let workout = createWorkouts()
-        
-        let databaseWorkout = ["Arctique" : ["April" : workout]]
-        
-        return databaseWorkout
-    }
-    
-    func createWorkoutLinesDatabase() -> [String: [WorkoutLine]] {
-        
-        let workoutLines = createWorkoutLines()
-        var databaseWorkoutLines = [String : [WorkoutLine]]()
-        
-        for index in 0...2 {
-            databaseWorkoutLines["testID" + "\(index)"] = workoutLines
-        }
-        
-        return databaseWorkoutLines
-    }
-    
-    func createGroup() -> Group {
-        return Group(groupName: "Arctique")
-    }
-    
-    func createError() -> WorkoutError {
-        return .empty
-    }
-    
     override func setUp() {
 
-        databaseWorkout = createWorkoutDatabase()
-        databaseWorkoutLines = createWorkoutLinesDatabase()
-        group = createGroup()
-        error = createError()
-        workoutManager = FirestoreWorkoutMock(databaseWorkout: databaseWorkout, databaseWorkoutLines: databaseWorkoutLines)
-        workoutManagerError = FirestoreWorkoutMock(databaseWorkout: databaseWorkout, databaseWorkoutLines: databaseWorkoutLines, error: error)
-        
+        database.initDatabase()
+        workoutManager = FirestoreWorkoutMock(databaseWorkout: database.databaseWorkout, databaseWorkoutLines: database.databaseWorkoutLines)
+        workoutManagerError = FirestoreWorkoutMock(databaseWorkout: database.databaseWorkout, databaseWorkoutLines: database.databaseWorkoutLines, error: database.error)
+
     }
-    
-    
     
     func testGivenWorkoutService_WhenFetchingWorkouts_ThenArrayEqualsDatabaseWorkouts() {
         // Given
         
         let expectation = XCTestExpectation(description: "Fetching Workout")
-        let workoutCount = databaseWorkout[group.groupName]?["April"]?.count
+        let workoutCount = database.databaseWorkout[database.group.groupName]?["April"]?.count
         
         // When
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             // Then
             
             XCTAssertNil(error)
@@ -118,7 +50,7 @@ class FirestoreWorkoutTest: XCTestCase {
         
         // When
         
-        workoutManagerError.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManagerError.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             // Then
             
             XCTAssertNotNil(error)
@@ -137,7 +69,7 @@ class FirestoreWorkoutTest: XCTestCase {
         
         // When
         
-        workoutManager.fetchWorkout(from: group, for: "Fail") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "Fail") { (workouts, error) in
             
             XCTAssertNil(error)
             XCTAssertEqual(workouts.count, 0)
@@ -155,11 +87,11 @@ class FirestoreWorkoutTest: XCTestCase {
         
         // When
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             // Then
             
             XCTAssertNil(error)
-            XCTAssertEqual(workoutLines.count, self.databaseWorkoutLines["testID0"]?.count)
+            XCTAssertEqual(workoutLines.count, self.database.databaseWorkoutLines["testID0"]?.count)
             expectation.fulfill()
         }
         
@@ -174,7 +106,7 @@ class FirestoreWorkoutTest: XCTestCase {
         
         // When
         
-        workoutManagerError.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManagerError.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             
             XCTAssertNotNil(error)
             XCTAssertEqual(workoutLines.count, 0)
@@ -191,23 +123,23 @@ class FirestoreWorkoutTest: XCTestCase {
         var countWorkout = 0
         var isPresent = false
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             countWorkout = workouts.count
         }
         
         // When
         
-        workoutManager.deleteWorkout(from: group, for: "April", workoutID: "testID0")
+        workoutManager.deleteWorkout(from: database.group, for: "April", workoutID: "testID0")
         
         // Then
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             
             XCTAssertEqual(workoutLines.count, 0)
             XCTAssertNil(error)
         }
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             
             XCTAssertEqual(workouts.count, countWorkout - 1)
             for workout in workouts {
@@ -226,17 +158,17 @@ class FirestoreWorkoutTest: XCTestCase {
         var countWorkoutLines = 0
         var isPresent = false
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             countWorkoutLines = workoutLines.count
         }
         
         // When
         
-        workoutManager.deleteWorkoutLine(from: group, for: "April", workoutID: "testID0", workoutLineID: "testID")
+        workoutManager.deleteWorkoutLine(from: database.group, for: "April", workoutID: "testID0", workoutLineID: "testID")
         
         // Then
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             
             XCTAssertEqual(workoutLines.count, countWorkoutLines - 1)
             
@@ -257,17 +189,17 @@ class FirestoreWorkoutTest: XCTestCase {
         
         let workout = Workout(title: "testTitle", date: "testDate", workoutID: "testID", workoutLines: [])
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             countWorkout = workouts.count
         }
         
         // When
         
-        let workoutID = workoutManager.addWorkout(to: group, for: "April", workout: workout)
+        let workoutID = workoutManager.addWorkout(to: database.group, for: "April", workout: workout)
         
         // Then
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             
             XCTAssertEqual(workouts.count, countWorkout + 1)
             XCTAssertEqual(workoutID, "testID")
@@ -281,17 +213,17 @@ class FirestoreWorkoutTest: XCTestCase {
         var countWorkout = 0
         let workoutWithoutID = Workout(title: "testTitle", date: "testDate", workoutID: "", workoutLines: [])
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             countWorkout = workouts.count
         }
         
         // When
         
-        let workoutID = workoutManager.addWorkout(to: group, for: "April", workout: workoutWithoutID)
+        let workoutID = workoutManager.addWorkout(to: database.group, for: "April", workout: workoutWithoutID)
         
         // Then
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             
             XCTAssertEqual(workouts.count, countWorkout + 1)
             XCTAssertEqual(workoutID, "test")
@@ -304,17 +236,17 @@ class FirestoreWorkoutTest: XCTestCase {
         
         var countWorkout = 0
         let workout = Workout(title: "testTitle", date: "testDate", workoutID: "testID0", workoutLines: [])
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             countWorkout = workouts.count
         }
         
         // When
         
-        let workoutID = workoutManager.addWorkout(to: group, for: "April", workout: workout)
+        let workoutID = workoutManager.addWorkout(to: database.group, for: "April", workout: workout)
         
         // Then
         
-        workoutManager.fetchWorkout(from: group, for: "April") { (workouts, error) in
+        workoutManager.fetchWorkout(from: database.group, for: "April") { (workouts, error) in
             
             XCTAssertEqual(workouts.count, countWorkout)
             XCTAssertEqual(workoutID, "testID0")
@@ -329,17 +261,17 @@ class FirestoreWorkoutTest: XCTestCase {
         var countWorkoutLines = 0
         let workoutLine = WorkoutLine(text: "test", zone1: 1.0, zone2: 1.0, zone3: 1.0, zone4: 1.0, zone5: 1.0, zone6: 1.0, zone7: 1.0, ampM: 1.0, coorM: 1.0, endM: 1.0, educ: 1.0, crawl: 1.0, medley: 1.0, spe: 1.0, nageC: 1.0, jbs: 1.0, bras: 1.0, workoutLineID: "testLineID", workoutLineTitle: "titleTest")
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             countWorkoutLines = workoutLines.count
         }
         
         // When
         
-        workoutManager.addWorkoutLine(to: group, for: "April", workoutID: "testID0", workoutLine: workoutLine)
+        workoutManager.addWorkoutLine(to: database.group, for: "April", workoutID: "testID0", workoutLine: workoutLine)
         
         // Then
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             
             XCTAssertEqual(workoutLines.count, countWorkoutLines + 1)
         }
@@ -352,17 +284,17 @@ class FirestoreWorkoutTest: XCTestCase {
         var countWorkoutLines = 0
         let workoutLine = WorkoutLine(text: "test", zone1: 1.0, zone2: 1.0, zone3: 1.0, zone4: 1.0, zone5: 1.0, zone6: 1.0, zone7: 1.0, ampM: 1.0, coorM: 1.0, endM: 1.0, educ: 1.0, crawl: 1.0, medley: 1.0, spe: 1.0, nageC: 1.0, jbs: 1.0, bras: 1.0, workoutLineID: "", workoutLineTitle: "titleTest")
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             countWorkoutLines = workoutLines.count
         }
         
         // When
         
-        workoutManager.addWorkoutLine(to: group, for: "April", workoutID: "testID0", workoutLine: workoutLine)
+        workoutManager.addWorkoutLine(to: database.group, for: "April", workoutID: "testID0", workoutLine: workoutLine)
         
         // Then
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             
             XCTAssertEqual(workoutLines.count, countWorkoutLines + 1)
             XCTAssertEqual(workoutLines[2].workoutLineID, "testLine")
@@ -377,17 +309,17 @@ class FirestoreWorkoutTest: XCTestCase {
         var countWorkoutLines = 0
         let workoutLine = WorkoutLine(text: "test + test", zone1: 1.0, zone2: 1.0, zone3: 1.0, zone4: 1.0, zone5: 1.0, zone6: 1.0, zone7: 1.0, ampM: 1.0, coorM: 1.0, endM: 1.0, educ: 1.0, crawl: 1.0, medley: 1.0, spe: 1.0, nageC: 1.0, jbs: 1.0, bras: 1.0, workoutLineID: "testID", workoutLineTitle: "titleTest")
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             countWorkoutLines = workoutLines.count
         }
         
         // When
         
-        workoutManager.addWorkoutLine(to: group, for: "April", workoutID: "testID0", workoutLine: workoutLine)
+        workoutManager.addWorkoutLine(to: database.group, for: "April", workoutID: "testID0", workoutLine: workoutLine)
         
         // Then
         
-        workoutManager.fetchWorkoutLines(from: group, for: "April", for: "testID0") { (workoutLines, error) in
+        workoutManager.fetchWorkoutLines(from: database.group, for: "April", for: "testID0") { (workoutLines, error) in
             
             XCTAssertEqual(workoutLines.count, countWorkoutLines)
             XCTAssertEqual(workoutLines[0].text, "test + test")
